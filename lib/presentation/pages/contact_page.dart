6a9c7d2b4df3bedefb50749271c9c8ad8738981c
EXTERNAL Harold Jose (Olisipo, TT-RHC/PAR-Av) <external.Jose.Harold@pt.bosch.com>
2022-07-16 00:38:31 +0100

    Finished Contact design page

--- lib/presentation/pages/contact_page.dart
+++ lib/presentation/pages/contact_page.dart
@@ -1,16 +1,75 @@
 import 'package:flutter/material.dart';
 
-class ContactPage extends StatelessWidget {
+import '../widgets/page_template_central.dart';
+import '../widgets/text_section_title.dart';
+
+class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
 
   @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text(
-          'Contact...',
-          style: TextStyle(fontSize: 35),
+    return PageTemplateCentral(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const TextSectionTitle("Get in Touch"),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your username',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8.0),
+                  TextFormField(
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    minLines: 10,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Your message',
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your message';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 8.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Sending message...')),
+                        );
+
+                        //TODO: harold -  Handle send data to backend
+                      }
+                    },
+                    child: const Text('Send'),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
